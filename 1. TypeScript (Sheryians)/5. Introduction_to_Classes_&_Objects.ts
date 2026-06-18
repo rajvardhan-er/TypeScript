c2decfde6b9103de451e71091c5d73fb03e6e36e
Rajvardhan <rajvardhan@softsnare.com>
2026-06-18 16:40:43 +0530

    Introduction to Classes & Objects

---
+++ 1. TypeScript (Sheryians)/5. Introduction_to_Classes_&_Objects.ts
@@ -0,0 +1,20 @@
+// 01:22:10 - 01:23:11 - Introduction to Classes & Objects
+
+//  - Class Definition
+//  - Constructors
+//  - Access modifiers (Public, Private, Protected)
+//  - Readonly properties
+//  - Optional properties
+//  - Parameter properties
+//  - Getters and setters
+//  - Static members
+//  - Abstract classes and Methods
+
+class Device {   // Class created using class keyword. eg. Device
+    name = "lg";
+    price = 12000;
+    category = "Digital";
+}
+
+let d1 = new Device();
+let d2 = new Device();
