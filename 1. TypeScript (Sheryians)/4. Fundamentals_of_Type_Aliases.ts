1bc5f248b926022d332e0e6e067a5553092e978f
Rajvardhan <rajvardhan@softsnare.com>
2026-06-18 16:13:44 +0530

    Fundamentals of Type Aliases

---
+++ 1. TypeScript (Sheryians)/4. Fundamentals_of_Type_Aliases.ts
@@ -0,0 +1,13 @@
+// 01:15:05 - 01:17:58 - Fundamentals of Type Aliases 
+
+// Type Aliases --> We can create own types
+
+type sankhya = number;
+let a: sankhya;
+
+type value = string | number | null;
+let b: value;
+
+// Intersection Types 
+
+let c: string | null;  // 1. Union
